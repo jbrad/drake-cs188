8bcfef7d1191433aa6bb0e83336cf5a7eae5168a
Jason Bradley <jason.bradley@me.com>
2020-02-17 20:16:16 -0600

    Crocs are so comfy!

--- index.js
+++ index.js
@@ -1,20 +1,139 @@
-const item = {
-    itemId: '02bbdbc7-e22e-4153-abd8-b5732a4ba6b5',
-    name: 'Ball cap',
-    description: 'Drake stuff',
-    price: 19.99,
-    size: 'Large'
+const Hapi = require('@hapi/hapi');
+const uuid = require('uuid');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    const samId = uuid.v4();
+    const crocodileSam = {
+        crocodileId: samId,
+        name: 'Sam',
+        age: 20
+    };
+
+    const crocodileSamantha = {
+        crocodileId: uuid.v4(),
+        name: 'Samantha',
+        age: 21
+    };
+
+    const crocodileDundie = {
+        crocodileId: uuid.v4(),
+        name: 'Dundie',
+        age: 55
+    };
+
+    let crocodiles = [crocodileSam, crocodileSamantha, crocodileDundie];
+
+    server.route({
+        method: 'GET',
+        path: '/crocodiles',
+        handler: (request, h) => {
+            return crocodiles;
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/crocodiles/{crocodileId}',
+        handler: (request, h) => {
+            const {crocodileId} = request.params;
+            const crocodile = crocodiles.find((croc) => croc.crocodileId === crocodileId);
+
+            if (!crocodile) {
+                return h.response().code(404);
+            }
+
+            return crocodile;
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/crocodiles',
+        handler: (request, h) => {
+            const crocodile = request.payload;
+            const existingCroc = crocodiles.find((croc) => croc.crocodileId === crocodile.crocodileId);
+
+            if (existingCroc) {
+                return h.response(existingCroc).code(303);
+            } else {
+                crocodiles.push(crocodile);
+
+                return h.response(crocodile).code(201);
+            }
+
+        }
+    });
+
+    server.route({
+        method: 'DELETE',
+        path: '/crocodiles/{crocodileId}',
+        handler: (request, h) => {
+            const {crocodileId} = request.params;
+            const crocodile = crocodiles.find((croc) => croc.crocodileId === crocodileId);
+
+            if (!crocodile) {
+                return h.response().code(404);
+            }
+
+            let newCrocodiles = [];
+
+            crocodiles.forEach((croc) => {
+                if (croc.crocodileId !== crocodileId) {
+                    newCrocodiles.push(croc);
+                }
+            });
+
+            crocodiles = newCrocodiles;
+
+            return '';
+        }
+    });
+
+    server.route({
+        method: 'PUT',
+        path: '/crocodiles/{crocodileId}',
+        handler: (request, h) => {
+            const {crocodileId} = request.params;
+            const updatedCrocodile = request.payload;
+
+            if (crocodileId === samId && updatedCrocodile.age !== 20) {
+                return h.response().code(422);
+            }
+
+            if (crocodileId !== updatedCrocodile.crocodileId) {
+                return h.response().code(409);
+            }
+
+            let newCrocodiles = [];
+
+            crocodiles.forEach((croc) => {
+                if (croc.crocodileId === crocodileId) {
+                    newCrocodiles.push(updatedCrocodile);
+                } else {
+                    newCrocodiles.push(croc);
+                }
+            });
+
+            crocodiles = newCrocodiles;
+
+            return '';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
 };
 
-const firstName = 'Jason';
-const lastName = 'Bradley';
+process.on('unhandledRejection', (err) => {
 
-const customer = {
-    firstName,
-    lastName,
-    email: `${firstName.toLowerCase()}.${lastName.toLowerCase()}@drake.edu`,
-    phoneNumber: '+15155555555'
-};
+    console.log(err);
+    process.exit(1);
+});
 
-console.log('item', item);
-console.log('customer', customer);
+init();

f2d74827beba55175090543fbd769559307dff4c
Jason Bradley <jason.bradley@me.com>
2020-03-11 00:03:56 -0500

    Fix lint in customer-service

--- services/customer-service.js
+++ services/customer-service.js
@@ -5,9 +5,9 @@ const {
 
 const mapToModel = (customer) => ({
     customerId: customer['customer_id'],
+    email: customer['email'],
     firstName: customer['first_name'],
-    lastName: customer['last_name'],
-    email: customer['email']
+    lastName: customer['last_name']
 });
 
 const getAllCustomers = () => {
@@ -18,6 +18,10 @@ const getAllCustomers = () => {
 
 const getCustomerByCustomerId = (customerId) => {
     const customer = selectCustomerByCustomerId(customerId);
+
+    if (!customer) {
+        return null;
+    }
 
     return mapToModel(customer);
 };

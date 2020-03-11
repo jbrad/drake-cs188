213c88c58515048d43cc54ecd7978a1eb971b38e
Jason Bradley <jason.bradley@me.com>
2020-03-11 00:05:16 -0500

    Lintz for customer-repository

--- repositories/customer-repository.js
+++ repositories/customer-repository.js
@@ -1,25 +1,20 @@
-const uuid = require('uuid');
-
-let customers = [
+const customers = [
     {
         'customer_id': 'd83ff143-9f8b-445a-8d8f-b9b8fe0f9f28',
+        'email': 'jason.bradley@drake.edu',
         'first_name': 'Jason',
-        'last_name': 'Bradley',
-        'email': 'jason.bradley@drake.edu'
+        'last_name': 'Bradley'
     }
 ];
 
 const selectCustomers = () => ({
-    rows: customers,
-    error: new Error(),
-    driver: 'postgres'
+    rows: customers
 });
 
 const selectCustomerByCustomerId = (customerId) =>
     customers.find((customer) => customer['customer_id'] === customerId);
 
-
 module.exports = {
-    selectCustomers,
-    selectCustomerByCustomerId
+    selectCustomerByCustomerId,
+    selectCustomers
 };

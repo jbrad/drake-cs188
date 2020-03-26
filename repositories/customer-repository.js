3803930d68f1db3ae28e62be1ff9f200ba7baf9b
Jason Bradley <jason.bradley@me.com>
2020-03-26 08:49:31 -0500

    Adds acceptance tests (#31)

--- repositories/customer-repository.js
+++ repositories/customer-repository.js
@@ -1,4 +1,4 @@
-const customers = [
+let customers = [
     {
         'customer_id': 'd83ff143-9f8b-445a-8d8f-b9b8fe0f9f28',
         'email': 'jason.bradley@drake.edu',
@@ -14,7 +14,29 @@ const selectCustomers = () => ({
 const selectCustomerByCustomerId = (customerId) =>
     customers.find((customer) => customer['customer_id'] === customerId);
 
+const insertCustomer = (customer) => customers.push(customer);
+
+const updateCustomer = (updatedCustomer) => {
+    const customersThatDontMatch = customers.filter((customer) =>
+        customer['customer_id'] !== updatedCustomer['customer_id']
+    );
+
+    customers = [
+        ...customersThatDontMatch,
+        updatedCustomer
+    ];
+};
+
+const deleteCustomerByCustomerId = (customerId) => {
+    customers = customers.filter((customer) =>
+        customer['customer_id'] !== customerId
+    );
+};
+
 module.exports = {
+    deleteCustomerByCustomerId,
+    insertCustomer,
     selectCustomerByCustomerId,
-    selectCustomers
+    selectCustomers,
+    updateCustomer
 };

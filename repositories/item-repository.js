b30d278e709a3ecd7db731a4d06f3c0ca70b99dc
Scott Molloy <scottm@webspecdesign.com>
2020-04-18 16:47:52 -0500

    Adds next.js capability and items landing page

--- repositories/item-repository.js
+++ repositories/item-repository.js
@@ -1,6 +1,7 @@
 let items = [
     {
         'description': 'Drake Sweatpants',
+        'image': 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F236x%2F1e%2F8d%2Fdc%2F1e8ddc92da776fb9a0fd80173fecf67c--sweatpants-drake.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fmckinleigh_12%2Fdrake-bulldogs%2F&tbnid=9oF2mgxTeYb0zM&vet=12ahUKEwi996ix-PLoAhXKf6wKHYZuDTEQMygDegUIARDsAQ..i&docid=2zqlurtTBozCXM&w=236&h=236&itg=1&q=drake%20sweatpants%20universit&safe=active&ved=2ahUKEwi996ix-PLoAhXKf6wKHYZuDTEQMygDegUIARDsAQ',
         'item_id': 'd83ff143-9f8b-445a-8d8f-b9b8fe0f9f29',
         'price': 30
     }
